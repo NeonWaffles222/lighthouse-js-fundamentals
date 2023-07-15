aa6a9dfc100ef8a948f5694b2e194753fe1323ea
Noah McMillan <noahm27@gmail.com>
2023-07-14 17:58:03 -0700

    A function to calulate how much syryp we can send to space

---
+++ spaceSyrup.js
@@ -0,0 +1,15 @@
+
+
+function howManyHundreds(num) {
+  let extra = num % 100;
+  num -= extra;
+  let hundreds = num / 100;
+
+  return hundreds;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
